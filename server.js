7f088817595bf33a969809c5ab7edc51e489d101
Sherb Win Home <anton.arbus@gmail.com>
2021-09-02 22:05:41 +0300

    first deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +9,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))

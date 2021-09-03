bd4395e8843e1947d5d9df3dd4da522ced81c46c
sherbsherb <sherbsherb@gmail.com>
2021-09-03 16:29:01 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Anton Arbus first React website
+          First React website for Anton Arbus
         </p>
       </header>
     </div>

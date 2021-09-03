ad6bbb5e82b3f379cf46f4ed49c63770d296de43
Sherb Win Home <anton.arbus@gmail.com>
2021-09-03 22:07:58 +0300

    comment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3010 // process.env.PORT || 80;
+const PORT = 3010; // process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
@@ -9,4 +9,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

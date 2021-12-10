12356358f31187a4a0cd3d27cae2d466a5e57261
Anton Windows Home <sherbsherb@gmail.com>
2021-12-10 21:37:22 +0200

    backdropFilter

--- src/components/PostsFeed/posts/backdropFilter.js
+++ src/components/PostsFeed/posts/backdropFilter.js
@@ -3,106 +3,33 @@ import styled from 'styled-components'
 
 function Cmpt() {
   const [blurState, SETblurState] = React.useState({ checked: true, cssVal: 4 })
-  const [brightnessState, SETbrightnessState] = React.useState({
-    checked: false,
-    cssVal: 160,
-  })
-  const [contrastState, SETcontrastState] = React.useState({
-    checked: false,
-    cssVal: 400,
-  })
-  const [dropShadowState, SETdropShadowState] = React.useState({
-    checked: false,
-    cssVal: '10px 10px 10px black',
-  })
-  const [grayscaleState, SETgrayscaleState] = React.useState({
-    checked: false,
-    cssVal: 90,
-  })
-  const [hueRotateState, SEThueRotateState] = React.useState({
-    checked: false,
-    cssVal: 120,
-  })
-  const [invertState, SETinvertState] = React.useState({
-    checked: false,
-    cssVal: 70,
-  })
-  const [opacityState, SETopacityState] = React.useState({
-    checked: false,
-    cssVal: 20,
-  })
-  const [sepiaState, SETsepiaState] = React.useState({
-    checked: false,
-    cssVal: 90,
-  })
-  const [saturateState, SETsaturateState] = React.useState({
-    checked: false,
-    cssVal: 80,
-  })
-
-  const backdropFilterCSS = {
-    backdropFilter: `
-      ${blurState.checked ? 'blur(' + blurState.cssVal + 'px)' : ''}  
-      ${
-        brightnessState.checked
-          ? 'brightness(' + brightnessState.cssVal + '%)'
-          : ''
-      }
-      ${contrastState.checked ? 'contrast(' + contrastState.cssVal + '%)' : ''}
-      ${
-        dropShadowState.checked
-          ? 'drop-shadow(' + dropShadowState.cssVal + ')'
-          : ''
-      }
-      ${
-        grayscaleState.checked
-          ? 'grayscale(' + grayscaleState.cssVal + '%)'
-          : ''
-      }
-      ${
-        hueRotateState.checked
-          ? 'hue-rotate(' + hueRotateState.cssVal + 'deg)'
-          : ''
-      }
-      ${invertState.checked ? 'invert(' + invertState.cssVal + '%)' : ''}
-      ${opacityState.checked ? 'opacity(' + opacityState.cssVal + '%)' : ''}
-      ${sepiaState.checked ? 'sepia(' + sepiaState.cssVal + '%)' : ''}
-      ${saturateState.checked ? 'saturate(' + saturateState.cssVal + '%)' : ''}
-    `,
-    WebkitBackdropFilter: `
-      ${blurState.checked ? 'blur(' + blurState.cssVal + 'px)' : ''}  
-      ${
-        brightnessState.checked
-          ? 'brightness(' + brightnessState.cssVal + '%)'
-          : ''
-      }
-      ${contrastState.checked ? 'contrast(' + contrastState.cssVal + '%)' : ''}
-      ${
-        dropShadowState.checked
-          ? 'drop-shadow(' + dropShadowState.cssVal + ')'
-          : ''
-      }
-      ${
-        grayscaleState.checked
-          ? 'grayscale(' + grayscaleState.cssVal + '%)'
-          : ''
-      }
-      ${
-        hueRotateState.checked
-          ? 'hue-rotate(' + hueRotateState.cssVal + 'deg)'
-          : ''
-      }
-      ${invertState.checked ? 'invert(' + invertState.cssVal + '%)' : ''}
-      ${opacityState.checked ? 'opacity(' + opacityState.cssVal + '%)' : ''}
-      ${sepiaState.checked ? 'sepia(' + sepiaState.cssVal + '%)' : ''}
-      ${saturateState.checked ? 'saturate(' + saturateState.cssVal + '%)' : ''}
-    `,
-  }
+  const [brightnessState, SETbrightnessState] = React.useState({ checked: false, cssVal: 160 })
+  const [contrastState, SETcontrastState] = React.useState({ checked: false, cssVal: 400 })
+  const [dropShadowState, SETdropShadowState] = React.useState({ checked: false, cssVal: '10px 10px 10px black' })
+  const [grayscaleState, SETgrayscaleState] = React.useState({ checked: false, cssVal: 90 })
+  const [hueRotateState, SEThueRotateState] = React.useState({ checked: false, cssVal: 120 })
+  const [invertState, SETinvertState] = React.useState({ checked: false, cssVal: 70 })
+  const [opacityState, SETopacityState] = React.useState({ checked: false, cssVal: 20 })
+  const [sepiaState, SETsepiaState] = React.useState({ checked: false, cssVal: 90 })
+  const [saturateState, SETsaturateState] = React.useState({ checked: false, cssVal: 80 })
 
   return (
     <>
       <Container>
-        <Box style={backdropFilterCSS}>Hello</Box>
+        <Box
+          blurState={blurState}
+          brightnessState={brightnessState}
+          contrastState={contrastState}
+          dropShadowState={dropShadowState}
+          grayscaleState={grayscaleState}
+          hueRotateState={hueRotateState}
+          invertState={invertState}
+          opacityState={opacityState}
+          sepiaState={sepiaState}
+          saturateState={saturateState}
+        >
+          Hello
+        </Box>
       </Container>
       <CSS>
         <div>{'.box {'}</div>
@@ -111,229 +38,72 @@ function Cmpt() {
           <div>backdrop-filter:</div>
           <div style={{ marginLeft: '20px' }}>
             <div>
-              <input
-                type="checkbox"
-                checked={blurState.checked}
-                onChange={() =>
-                  SETblurState({ ...blurState, checked: !blurState.checked })
-                }
-              />
+              <input type="checkbox" checked={blurState.checked} onChange={() => SETblurState({ ...blurState, checked: !blurState.checked }) } />
               blur(
-              <input
-                type="number"
-                onChange={e =>
-                  SETblurState({ ...blurState, cssVal: e.target.value })
-                }
-                value={blurState.cssVal}
-              />
+              <input type="number" onChange={e => SETblurState({ ...blurState, cssVal: e.target.value }) } value={blurState.cssVal} />
               px)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={brightnessState.checked}
-                onChange={() =>
-                  SETbrightnessState({
-                    ...brightnessState,
-                    checked: !brightnessState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={brightnessState.checked} onChange={() => SETbrightnessState({ ...brightnessState, checked: !brightnessState.checked }) } />
               brightness(
-              <input
-                type="number"
-                onChange={e =>
-                  SETbrightnessState({
-                    ...brightnessState,
-                    cssVal: e.target.value,
-                  })
-                }
-                value={brightnessState.cssVal}
-              />
+              <input type="number" onChange={e => SETbrightnessState({ ...brightnessState, cssVal: e.target.value }) } value={brightnessState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={contrastState.checked}
-                onChange={() =>
-                  SETcontrastState({
-                    ...contrastState,
-                    checked: !contrastState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={contrastState.checked} onChange={() => SETcontrastState({ ...contrastState, checked: !contrastState.checked }) } />
               contrast(
-              <input
-                type="number"
-                onChange={e =>
-                  SETcontrastState({ ...contrastState, cssVal: e.target.value })
-                }
-                value={contrastState.cssVal}
-              />
+              <input type="number" onChange={e => SETcontrastState({ ...contrastState, cssVal: e.target.value }) } value={contrastState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={dropShadowState.checked}
-                onChange={() =>
-                  SETdropShadowState({
-                    ...dropShadowState,
-                    checked: !dropShadowState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={dropShadowState.checked} onChange={() => SETdropShadowState({ ...dropShadowState, checked: !dropShadowState.checked }) } />
               drop-shadow(
-              <input
-                type="text"
-                onChange={e =>
-                  SETdropShadowState({
-                    ...dropShadowState,
-                    cssVal: e.target.value,
-                  })
-                }
-                value={dropShadowState.cssVal}
-                style={{ maxWidth: '150px' }}
-              />
+              <input type="text" onChange={e => SETdropShadowState({ ...dropShadowState, cssVal: e.target.value }) } value={dropShadowState.cssVal} style={{ maxWidth: '150px' }} />
               )  <span style={{ color: 'red' }}>not working, no clue why</span>
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={grayscaleState.checked}
-                onChange={() =>
-                  SETgrayscaleState({
-                    ...grayscaleState,
-                    checked: !grayscaleState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={grayscaleState.checked} onChange={() => SETgrayscaleState({ ...grayscaleState, checked: !grayscaleState.checked }) } />
               grayscale(
-              <input
-                type="number"
-                onChange={e =>
-                  SETgrayscaleState({
-                    ...grayscaleState,
-                    cssVal: e.target.value,
-                  })
-                }
-                value={grayscaleState.cssVal}
-              />
+              <input type="number" onChange={e => SETgrayscaleState({ ...grayscaleState, cssVal: e.target.value }) } value={grayscaleState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={hueRotateState.checked}
-                onChange={() =>
-                  SEThueRotateState({
-                    ...hueRotateState,
-                    checked: !hueRotateState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={hueRotateState.checked} onChange={() => SEThueRotateState({ ...hueRotateState, checked: !hueRotateState.checked }) } />
               hue-rotate(
-              <input
-                type="number"
-                onChange={e =>
-                  SEThueRotateState({
-                    ...hueRotateState,
-                    cssVal: e.target.value,
-                  })
-                }
-                value={hueRotateState.cssVal}
-              />
+              <input type="number" onChange={e => SEThueRotateState({ ...hueRotateState, cssVal: e.target.value }) } value={hueRotateState.cssVal} />
               deg)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={invertState.checked}
-                onChange={() =>
-                  SETinvertState({
-                    ...invertState,
-                    checked: !invertState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={invertState.checked} onChange={() => SETinvertState({ ...invertState, checked: !invertState.checked }) } />
               invert(
-              <input
-                type="number"
-                onChange={e =>
-                  SETinvertState({ ...invertState, cssVal: e.target.value })
-                }
-                value={invertState.cssVal}
-              />
+              <input type="number" onChange={e => SETinvertState({ ...invertState, cssVal: e.target.value }) } value={invertState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={opacityState.checked}
-                onChange={() =>
-                  SETopacityState({
-                    ...opacityState,
-                    checked: !opacityState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={opacityState.checked} onChange={() => SETopacityState({ ...opacityState, checked: !opacityState.checked }) } />
               opacity(
-              <input
-                type="number"
-                onChange={e =>
-                  SETopacityState({ ...opacityState, cssVal: e.target.value })
-                }
-                value={opacityState.cssVal}
-              />
+              <input type="number" onChange={e => SETopacityState({ ...opacityState, cssVal: e.target.value }) } value={opacityState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={sepiaState.checked}
-                onChange={() =>
-                  SETsepiaState({ ...sepiaState, checked: !sepiaState.checked })
-                }
-              />
+              <input type="checkbox" checked={sepiaState.checked} onChange={() => SETsepiaState({ ...sepiaState, checked: !sepiaState.checked }) } />
               sepia(
-              <input
-                type="number"
-                onChange={e =>
-                  SETsepiaState({ ...sepiaState, cssVal: e.target.value })
-                }
-                value={sepiaState.cssVal}
-              />
+              <input type="number" onChange={e => SETsepiaState({ ...sepiaState, cssVal: e.target.value }) } value={sepiaState.cssVal} />
               %)
             </div>
 
             <div>
-              <input
-                type="checkbox"
-                checked={saturateState.checked}
-                onChange={() =>
-                  SETsaturateState({
-                    ...saturateState,
-                    checked: !saturateState.checked,
-                  })
-                }
-              />
+              <input type="checkbox" checked={saturateState.checked} onChange={() => SETsaturateState({ ...saturateState, checked: !saturateState.checked }) } />
               saturate(
-              <input
-                type="number"
-                onChange={e =>
-                  SETsaturateState({ ...saturateState, cssVal: e.target.value })
-                }
-                value={saturateState.cssVal}
-              />
+              <input type="number" onChange={e => SETsaturateState({ ...saturateState, cssVal: e.target.value }) } value={saturateState.cssVal} />
               %)
             </div>
           </div>
@@ -364,6 +134,17 @@ const Box = styled.div`
   width: 50%;
   height: 50%;
   background-color: rgb(255 255 255 / 50%);
+  backdrop-filter:  
+    ${props => props.blurState.checked ? 'blur(' + props.blurState.cssVal + 'px)' : ''}  
+    ${props => props.brightnessState.checked ? 'brightness(' + props.brightnessState.cssVal + '%)' : ''}
+    ${props => props.contrastState.checked ? 'contrast(' + props.contrastState.cssVal + '%)' : ''}
+    ${props => props.dropShadowState.checked ? 'drop-shadow(' + props.dropShadowState.cssVal + ')' : ''}
+    ${props => props.grayscaleState.checked ? 'grayscale(' + props.grayscaleState.cssVal + '%)' : ''}
+    ${props => props.hueRotateState.checked ? 'hue-rotate(' + props.hueRotateState.cssVal + 'deg)' : ''}
+    ${props => props.invertState.checked ? 'invert(' + props.invertState.cssVal + '%)' : ''}
+    ${props => props.opacityState.checked ? 'opacity(' + props.opacityState.cssVal + '%)' : ''}
+    ${props => props.sepiaState.checked ? 'sepia(' + props.sepiaState.cssVal + '%)' : ''}
+    ${props => props.saturateState.checked ? 'saturate(' + props.saturateState.cssVal + '%)' : ''}
 `
 
 const CSS = styled.div`
